b6758510456db9ca6b5ac772fe1a204b8368ecf1
arsenio2012 <arseniomora2001@gmail.com>
2023-07-16 08:40:15 -0400

    Actualizando

--- segunda_app/app.py
+++ segunda_app/app.py
@@ -1,12 +1,39 @@
 import tkinter as tk
+import mysql.connector
 from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+
+def opcion1_click():
+    etiqueta_titulo.config(text="Opcion 1")
+    contenido.config(text="Aqui va el contenido de la opcion 1")
+
+def opcion2_click():
+    etiqueta_titulo.config(text="Opción 2")
+    contenido.config(text="Aquí va el contenido de la opción 2")
+
+def opcion3_click():
+    etiqueta_titulo.config(text="Opción 3")
+    contenido.config(text="Aquí va el contenido de la opción 3")
 
 def iniciar_sesion():
     usuario = usuario_entry.get()
     contrasena = contrasena_entry.get()
 
-    # Verificar las credenciales de inicio de sesión
-    if usuario == "admin" and contrasena == "123456":
+     #Establemos una conexón a la base de datos o la creamos si no existe
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="python_test"
+    )
+
+    cursor = conexion.cursor()
+    cursor.execute("SELECT username FROM users WHERE username = %s AND password = %s", (usuario, contrasena))
+    resultado = cursor.fetchone()
+    conexion.close()
+
+    if resultado is not None:
         messagebox.showinfo("Inicio de sesión exitoso", f"Bienvenido, {usuario}!")
         ventana.withdraw()  # Ocultar la ventana de inicio de sesión
 
@@ -14,6 +41,13 @@ def iniciar_sesion():
         ventana_dashboard = tk.Toplevel()
         ventana_dashboard.title("Dashboard")
         ventana_dashboard.configure(bg="white")
+
+        # Obtener el ancho y alto de la pantalla
+        ancho_pantalla = ventana_dashboard.winfo_screenwidth()
+        alto_pantalla = ventana_dashboard.winfo_screenheight()
+
+        # Establecer el tamaño y posición de la ventana
+        ventana_dashboard.geometry(f"{ancho_pantalla}x{alto_pantalla}+0+0")
 
         # Panel vertical izquierdo (Menú)
         panel_izquierdo = tk.Frame(ventana_dashboard, bg="light gray")
@@ -24,15 +58,15 @@ def iniciar_sesion():
         etiqueta_menu.pack(pady=10)
 
         # Opción 1
-        opcion1 = tk.Button(panel_izquierdo, text="Opción 1", bg="white", padx=10, pady=5)
+        opcion1 = tk.Button(panel_izquierdo, text="Opción 1", bg="white", padx=10, pady=5, command=opcion1_click)
         opcion1.pack(pady=5)
 
         # Opción 2
-        opcion2 = tk.Button(panel_izquierdo, text="Opción 2", bg="white", padx=10, pady=5)
+        opcion2 = tk.Button(panel_izquierdo, text="Opción 2", bg="white", padx=10, pady=5, command=opcion2_click)
         opcion2.pack(pady=5)
 
         # Opción 3
-        opcion3 = tk.Button(panel_izquierdo, text="Opción 3", bg="white", padx=10, pady=5)
+        opcion3 = tk.Button(panel_izquierdo, text="Opción 3", bg="white", padx=10, pady=5, command=opcion3_click)
         opcion3.pack(pady=5)
 
         # Contenedor a la derecha
@@ -40,6 +74,7 @@ def iniciar_sesion():
         contenedor_derecho.pack(side="right", fill="both", expand=True)
 
         # Agregar elementos al contenedor derecho
+        global etiqueta_titulo, contenido
         etiqueta_titulo = tk.Label(contenedor_derecho, text="Contenido", bg="white", font=("Arial", 16, "bold"))
         etiqueta_titulo.pack(pady=10)
 
@@ -48,19 +83,21 @@ def iniciar_sesion():
 
         ventana_dashboard.mainloop()
     else:
-        messagebox.showerror("Inicio de sesión fallido", "Credenciales incorrectas")
+       messagebox.showerror("Inicio de sesión fallido", "Credenciales incorrectas") 
 
 # Crear la ventana de inicio de sesión
 ventana = tk.Tk()
 ventana.title("Dpworkapp")
 ventana.configure(bg="black")
 
-# Obtener el ancho y alto de la pantalla
-ancho_pantalla = ventana.winfo_screenwidth()
-alto_pantalla = ventana.winfo_screenheight()
+# Establecer el tamaño 
+ventana.geometry(f"800x400")
 
-# Establecer el tamaño y posición de la ventana
-ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}+0+0")
+# Ruta del archivo de icono (.ico)
+#ruta_icono = "recursos/imagenes/dp.ico"
+
+# Establecer el icono de la ventana
+#ventana.iconbitmap(ruta_icono)
 
 # Agregar un marco que ocupe todo el espacio
 marco = tk.Frame(ventana, bg="black")
